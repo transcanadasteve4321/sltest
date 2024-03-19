5a92b0c530bfcd3e95a084982d8a3328a54477bf
transcanadasteve4321 <transcanada_steve@hotmail.com>
2024-03-19 11:02:24 -0600

    Update init.tf again

--- init.tf
+++ init.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region      = "us-west-2"
-  access_key  = var.TF_VAR_aws_access_key
-  secret_key  = var.TF_VAR_aws_secret_key
+  access_key  = var.aws_access_key
+  secret_key  = var.aws_secret_key
   max_retries = 100
 }
